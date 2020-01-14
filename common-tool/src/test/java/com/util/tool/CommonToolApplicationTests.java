9dc7ab16e1a1fae9c5a6e308651fd564fc0a157a
zhaojun <1607729805@qq.com>
2020-01-14 17:36:34 +0800

    project init

---
+++ common-tool/src/test/java/com/util/tool/CommonToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.util.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
